43e8b7399dcb530e8065dc2f1600774dabf885dd
GudingStar <110057265+GudingStar@users.noreply.github.com>
2023-02-24 07:31:04 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ pages/index.tsx
@@ -0,0 +1,27 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import { Inter } from 'next/font/google'
+import { ConnectKitButton } from "connectkit";
+import styles from '@/styles/Home.module.css'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export default function Home() {
+  return (
+    <>
+    <div className="flex justify-center h-[100vh] w-full">
+      <div className="flex">
+      <ConnectKitButton.Custom>
+      {({ isConnected, show, truncatedAddress, ensName }) => {
+        return (
+          <div className="connectButton" onClick={show}>
+            {isConnected ? ensName ?? truncatedAddress : "Connect Wallet"}
+          </div>
+        );
+      }}   
+      </ConnectKitButton.Custom>
+    </div>
+    </div>
+    </>
+  )
+}
